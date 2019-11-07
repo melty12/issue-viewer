8b127d661fe37f48ea3b707f653302c1d225ba94
melty12 <komei.1220@gmail.com>
2019-11-07 16:43:16 +0900

    fix: setting HMR redux Provider in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import store from './app/store'
 import './index.css'
-import App from './app/App'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const render = () => {
+  const App = require('./app/App').default
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  )
+}
+
+render()
+
+if(process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./app/App', render)
+}
