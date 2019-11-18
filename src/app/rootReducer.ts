93692a852f22b6123062498b4bf67ace1397d5f4
melty12 <komei.1220@gmail.com>
2019-11-18 13:16:42 +0900

    Add: a slice for tracking issues state

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import issuesDisplaySlice from "../features/issuesDisplay/issuesDisplaySlice";
 import repoDetailsReducer from "../features/repoSearch/repoDetailsSlice";
+import issuesReducer from "../features/issuesList/issuesSlice";
 
 const rootReducer = combineReducers({
   issueDisplay: issuesDisplaySlice,
-  repoDetails: repoDetailsReducer
+  repoDetails: repoDetailsReducer,
+  issues: issuesReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
