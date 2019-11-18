9b532bef87e47791d8a84d7bb08349bf728afbfd
melty12 <komei.1220@gmail.com>
2019-11-18 13:44:34 +0900

    Add: a slice for tracking comments data

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import issuesDisplaySlice from "../features/issuesDisplay/issuesDisplaySlice";
 import repoDetailsReducer from "../features/repoSearch/repoDetailsSlice";
 import issuesReducer from "../features/issuesList/issuesSlice";
+import commentsReducer from "../features/issueDetails/commentsSlice";
 
 const rootReducer = combineReducers({
   issueDisplay: issuesDisplaySlice,
   repoDetails: repoDetailsReducer,
-  issues: issuesReducer
+  issues: issuesReducer,
+  comments: commentsReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
