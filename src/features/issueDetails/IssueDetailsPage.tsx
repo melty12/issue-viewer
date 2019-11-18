320414f78db3fd22714b7871a7bb35d323532cc8
melty12 <komei.1220@gmail.com>
2019-11-18 13:27:42 +0900

    Update: IssueDetailsPage to fetch issue data via Redux

--- src/features/issueDetails/IssueDetailsPage.tsx
+++ src/features/issueDetails/IssueDetailsPage.tsx
@@ -1,22 +1,26 @@
-import React, { useState, useEffect } from 'react'
-import ReactMarkdown from 'react-markdown'
-import classnames from 'classnames'
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import ReactMarkdown from "react-markdown";
+import classnames from "classnames";
 
-import { insertMentionLinks } from '../../utils/stringUtils'
-import { getIssue, getComments, Issue, Comment } from '../../api/githubAPI'
-import { IssueLabels } from '../../components/IssueLabels'
+import { insertMentionLinks } from "../../utils/stringUtils";
+import { getComments, Comment } from "../../api/githubAPI";
+import { IssueLabels } from "../../components/IssueLabels";
 
-import { IssueMeta } from './IssueMeta'
-import { IssueComments } from './IssueComments'
+import { RootState } from "../../app/rootReducer";
+import { fetchIssue } from "../../features/issuesList/issuesSlice";
 
-import styles from './IssueDetailsPage.module.css'
-import './IssueDetailsPage.css'
+import { IssueMeta } from "./IssueMeta";
+import { IssueComments } from "./IssueComments";
+
+import styles from "./IssueDetailsPage.module.css";
+import "./IssueDetailsPage.css";
 
 interface IDProps {
-  org: string
-  repo: string
-  issueId: number
-  showIssuesList: () => void
+  org: string;
+  repo: string;
+  issueId: number;
+  showIssuesList: () => void;
 }
 
 export const IssueDetailsPage = ({
@@ -25,42 +29,40 @@ export const IssueDetailsPage = ({
   issueId,
   showIssuesList
 }: IDProps) => {
-  const [issue, setIssue] = useState<Issue | null>(null)
-  const [comments, setComments] = useState<Comment[]>([])
-  const [commentsError, setCommentsError] = useState<Error | null>(null)
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [commentsError] = useState<Error | null>(null);
+
+  const dispatch = useDispatch();
+
+  const issue = useSelector(
+    (state: RootState) => state.issues.issuesByNumber[issueId]
+  );
 
   useEffect(() => {
-    async function fetchIssue() {
-      try {
-        setCommentsError(null)
-        const issue = await getIssue(org, repo, issueId)
-        setIssue(issue)
-      } catch (err) {
-        setCommentsError(err)
-      }
+    if (!issue) {
+      dispatch(fetchIssue(org, repo, issueId));
     }
-
-    fetchIssue()
-  }, [org, repo, issueId])
+    window.scrollTo({ top: 0 });
+  }, [org, repo, issueId, issue, dispatch]);
 
   useEffect(() => {
     async function fetchComments() {
       if (issue !== null) {
-        const comments = await getComments(issue.comments_url)
-        setComments(comments)
+        const comments = await getComments(issue.comments_url);
+        setComments(comments);
       }
     }
 
-    fetchComments()
-  }, [issue])
+    fetchComments();
+  }, [issue]);
 
-  let content
+  let content;
 
   const backToIssueListButton = (
     <button className="pure-button" onClick={showIssuesList}>
       Back to Issues List
     </button>
-  )
+  );
 
   if (commentsError) {
     return (
@@ -69,7 +71,7 @@ export const IssueDetailsPage = ({
         <h1>There was a problem loading issue #{issueId}</h1>
         <p>{commentsError.toString()}</p>
       </div>
-    )
+    );
   }
 
   if (issue === null) {
@@ -78,12 +80,12 @@ export const IssueDetailsPage = ({
         {backToIssueListButton}
         <p>Loading issue #{issueId}...</p>
       </div>
-    )
+    );
   } else {
-    let renderedComments = <IssueComments issue={issue} comments={comments} />
+    let renderedComments = <IssueComments issue={issue} comments={comments} />;
 
     content = (
-      <div className={classnames('issueDetailsPage', styles.issueDetailsPage)}>
+      <div className={classnames("issueDetailsPage", styles.issueDetailsPage)}>
         <h1 className="issue-detail__title">{issue.title}</h1>
         {backToIssueListButton}
         <IssueMeta issue={issue} />
@@ -91,15 +93,15 @@ export const IssueDetailsPage = ({
         <hr className={styles.divider} />
         <div className={styles.summary}>
           <ReactMarkdown
-            className={'testing'}
+            className={"testing"}
             source={insertMentionLinks(issue.body)}
           />
         </div>
         <hr className={styles.divider} />
         <ul>{renderedComments}</ul>
       </div>
-    )
+    );
   }
 
-  return <div>{content}</div>
-}
+  return <div>{content}</div>;
+};
